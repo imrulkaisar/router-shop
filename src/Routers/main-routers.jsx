d5e822392f8cf6ca35e220a65b99787812d04546
imrulkaisar <imrulkaisar3@gmail.com>
2023-09-22 07:36:53 +0600

    Added main page routers

--- src/Routers/main-routers.jsx
+++ src/Routers/main-routers.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import PageTemplate from "../Layouts/PageTemplate";
 import Home from "../Pages/Home";
+import Products from "../Pages/Products";
+import Dashboard from "../Layouts/Dashboard";
 
 const routers = createBrowserRouter([
   {
@@ -13,9 +15,23 @@ const routers = createBrowserRouter([
       },
       {
         path: "/products",
-        element: <h1>Products page.</h1>,
+        element: <Products />,
+      },
+      {
+        path: "/about",
+        element: <h1 className="text-4xl font-bold text-center">About page</h1>,
+      },
+      {
+        path: "/contact",
+        element: (
+          <h1 className="text-4xl font-bold text-center">Contact page</h1>
+        ),
       },
     ],
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
   },
 ]);
 
